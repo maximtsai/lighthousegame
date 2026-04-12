0ff68befcfd5207f83dd738a6db59e43cffecff8
Maxim Tsai <maximtsai@yahoo.com>
2026-04-12 12:40:23 -0700

    game restarts properly

--- Assets/Scripts/MainMenuLogic.cs
+++ Assets/Scripts/MainMenuLogic.cs
@@ -31,6 +31,7 @@ public class MainMenuLogic : MonoBehaviour
 
     public void StartGame()
     {
+        GameState.FullReset();
         // TODO: Change this to 1 for any release build
         int startDay = 1;
         GameState.Set("day", startDay);
